452e3fe07f0b48463cea8c40a0531a958b9db9b3
Haley-Zhu <Haley.ZhuY@gmail.com>
2020-05-28 00:12:24 +1000

    config env, using dotenv

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send("use routes");
+  res.send("use routes on 4000");
 })
 
 module.exports = router;
