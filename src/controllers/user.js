7f5ca6dd603c59c8795f57679da7c906020cfd64
Haley-Zhu <Haley.ZhuY@gmail.com>
2020-06-02 00:03:26 +1000

    complete basic CURD of user

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,31 +1,82 @@
-const User = require('../models/user');
+const User = require("../models/user");
 
-async function addUser(req, res){
+async function addUser(req, res) {
   const { name, password } = req.body;
+
+  const existingUser = await User.findOne({ name });
+  if (existingUser) {
+    return res.status(400).json("user has already existed");
+  }
+
   const user = new User({
     name,
-    password
+    password,
   });
-  
   await user.save();
+  if (!user) {
+    return res.status(500).json("adding user failed");
+  }
+
   return res.json(user);
 }
 
-function getUser(req, res){}
+async function getUser(req, res) {
+  const { id } = req.params;
+  const user = await User.findById(id).exec();
 
-async function getAllUsers(req, res){
-  const users = await User.find();
+  if (!user) {
+    return res.status(404).json("user is not found");
+  }
+
+  return res.json(user);
+}
+
+async function getAllUsers(req, res) {
+  const users = await User.find().exec();
+  if (!users) {
+    return res.status(404).json("users are not found");
+  }
   return res.json(users);
 }
 
-function updateUser(req, res){}
+async function updateUser(req, res) {
+  const { id } = req.params;
+  const { name, password } = req.body;
 
-function deleteUser(req, res){}
+  const existingUser = await User.findById(id);
+  if (!existingUser) {
+    return res.status(404).json("user is not found");
+  }
+  if (name !== existingUser.name) {
+    return res.status(400).json("username cannot be changed");
+  }
+
+  const updatedUser = await User.findByIdAndUpdate(
+    id,
+    { name, password },
+    { new: true }
+  );
+  if (!updatedUser) {
+    return res.status(500).json("updating user failed");
+  }
+  return res.json(updatedUser);
+}
+
+async function deleteUser(req, res) {
+  const { id } = req.params;
+
+  const deletedUser = await User.findByIdAndRemove(id);
+  if (!deletedUser) {
+    return res.status(404).json("user is not found");
+  }
+
+  return res.sendStatus(200);
+}
 
 module.exports = {
   addUser,
   getUser,
   getAllUsers,
   updateUser,
-  deleteUser
-}
+  deleteUser,
+};
